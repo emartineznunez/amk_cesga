a3730ac15c8e98c4a3c611f5509a49bf3127ec3d
Emilio Martínez Núñez <50674314+emartineznunez@users.noreply.github.com>
2024-02-09 11:14:43 +0100

    Add files via upload

---
+++ install_automekin/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Installing automekin
+module load ase
+rm -rf AutoMeKin
+git clone https://github.com/emartineznunez/AutoMeKin.git 
+cd AutoMeKin
+autoreconf -i
+./configure
+make 
+make install
+#Changing module name
+cp $HOME/amk-2021/modules/amk/2021 $HOME/amk-2021/modules/amk/2021local
+#PATCH for FT-III
+sed -i 's@srun -N1@srun --exclusive -N1@g;s@corespertask --cpu_bind=none@corespertask@g' $HOME/amk-2021/bin/utils.sh
+#Installing amk_tools
+cd $HOME
+rm -rf amk_tools
+git clone https://github.com/dgarayr/amk_tools.git
+cd amk_tools
+pip install -e .
+wget -r $path_to_dir .
---
+++ install_automekin/module_load.sh
@@ -0,0 +1,1 @@
+module load gcccore/system amk/2021local nodejs/14.17.6 bokeh/2.4.2 ase

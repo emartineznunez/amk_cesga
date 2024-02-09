cf28e7a33ba5f54f1825d52c3d40619bd2d2cc25
Emilio Martínez Núñez <50674314+emartineznunez@users.noreply.github.com>
2024-02-09 12:39:58 +0100

    Update install.sh

--- install_automekin/install.sh
+++ install_automekin/install.sh
@@ -18,4 +18,4 @@ rm -rf amk_tools
 git clone https://github.com/dgarayr/amk_tools.git
 cd amk_tools
 pip install -e .
-wget -r $path_to_dir .
+wget -r https://raw.githubusercontent.com/emartineznunez/amk_cesga/main/install_automekin/modules/ .
